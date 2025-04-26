3b0236b1f7c1e8622d42ac4194c49643541549c5
Nathaniel <nathanielotoole@gmail.com>
2025-04-26 14:12:22 -0400

    drink-post-content pattern

---
+++ uc-theme-ui/patterns/drink-post-content.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Title: Drink Post Content
+ * Slug: uc-theme-ui/drink-post-content
+ * Description: A pattern for displaying drink content with image and details
+ * Categories: drinks, content
+ * Keywords: drink, cocktail, recipe, details
+ * Viewport Width: 1200
+ * Block Types: core/post-content
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+    <!-- wp:media-text {"mediaId":0,"mediaLink":"","mediaType":"image"} -->
+    <div class="wp-block-media-text alignwide is-stacked-on-mobile">
+        <figure class="wp-block-media-text__media">
+            <img src="" alt="" class="wp-image-0" />
+        </figure>
+        
+        <div class="wp-block-media-text__content">
+            <!-- wp:heading {"level":1} -->
+            <h1>Drink Name</h1>
+            <!-- /wp:heading -->
+
+            <!-- wp:list -->
+            <ul>
+                <li><em>Category</em>: </li>
+                <li><em>Color</em>: </li>
+                <li><em>Glass</em>: </li>
+                <li><em>Garnish</em>: </li>
+                <li><em>Base</em>: </li>
+                <li><em>Ice</em>: </li>
+            </ul>
+            <!-- /wp:list -->
+        </div>
+    </div>
+    <!-- /wp:media-text -->
+</div>
+<!-- /wp:group -->
